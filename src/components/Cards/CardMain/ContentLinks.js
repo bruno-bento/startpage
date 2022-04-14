b6810f528d50e468cfcbf3a932a922099e8baa34
bruno-bento <brunobentoreinoso14@gmail.com>
2022-04-13 22:16:20 -0300

    Inclusão dos links com responsividade

--- src/components/Cards/CardMain/ContentLinks.js
+++ src/components/Cards/CardMain/ContentLinks.js
@@ -1,9 +1,13 @@
 import './ContentLinks.css';
-
+import Links from '../../Links/Links';
 const ContentLinks = () => {
     return (
         <div className="card card-content-links">
-
+            <Links></Links>
+            <Links></Links>
+            <Links></Links>
+            <Links></Links>
+            <Links></Links>
         </div>
     );
 }
---
+++ src/components/Links/Links.js
@@ -0,0 +1,26 @@
+import "./Links.css";
+
+const Links = () => {
+  return (
+    <div className="card-links">
+      <div className="card-header">
+        <h3>Links</h3>
+      </div>
+      <div className="card-body">
+        <ul className="list-group">
+          <a href="#www." alt="link" className="list-group-item"> Item</a>
+          <a href="#www." alt="link" className="list-group-item"> Item</a>
+          <a href="#www." alt="link" className="list-group-item"> Item</a>
+          <a href="#www." alt="link" className="list-group-item"> Item</a>
+          <a href="#www." alt="link" className="list-group-item"> Item</a>
+          <a href="#www." alt="link" className="list-group-item"> Item</a>
+          <a href="#www." alt="link" className="list-group-item"> Item</a>
+          <a href="#www." alt="link" className="list-group-item"> Item</a>
+          <a href="#www." alt="link" className="list-group-item"> Item</a>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Links;
