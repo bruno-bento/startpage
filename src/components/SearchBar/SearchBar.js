1b20ff91553c46510cc169df4ed0f244436c1981
bruno-bento <brunobentoreinoso14@gmail.com>
2022-04-14 08:29:38 -0300

    custom scroll bar and fixed search btn

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,9 +2,11 @@ import "./SearchBar.css";
 import { HiSearch } from "react-icons/hi";
 const SearchBar = (props) => {
   return (
-    <div className="card divSearchBar">
-      <HiSearch color="#38BDF8"/>
-      <input type="text" placeholder="g/you-search-here" />
+    <div className="card div-search-bar">
+      <button className="btn-search-bar">
+        <HiSearch color="#38BDF8"/>
+      </button>
+      <input className="input-search-bar" type="text" placeholder="g/you-search-here" />
     </div>
   );
 };
