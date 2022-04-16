15b1b5f2f8fb8404fcb57037bed6693e96d4c8f9
bruno-bento <brunobentoreinoso14@gmail.com>
2022-04-16 11:41:15 -0300

    Search in google and duckduckgo

--- src/components/Links/Links.js
+++ src/components/Links/Links.js
@@ -1,22 +1,20 @@
 import "./Links.css";
 
-const Links = () => {
+const Links = (props) => {
   return (
     <div className="card-links">
       <div className="card-header">
-        <h3>Links</h3>
+        <h3>d/Dev</h3>
       </div>
       <div className="card-body">
         <ul className="list-group">
-          <a href="#www." alt="link" className="list-group-item"> Item</a>
-          <a href="#www." alt="link" className="list-group-item"> Item</a>
-          <a href="#www." alt="link" className="list-group-item"> Item</a>
-          <a href="#www." alt="link" className="list-group-item"> Item</a>
-          <a href="#www." alt="link" className="list-group-item"> Item</a>
-          <a href="#www." alt="link" className="list-group-item"> Item</a>
-          <a href="#www." alt="link" className="list-group-item"> Item</a>
-          <a href="#www." alt="link" className="list-group-item"> Item</a>
-          <a href="#www." alt="link" className="list-group-item"> Item</a>
+          <a href="https://github.com/bruno-bento" alt="link" className="list-group-item"> GitHub</a>
+          <a href="https://stackoverflow.com/" alt="link" className="list-group-item"> StackOverflow</a>
+          <a href="https://www.figma.com/files/" alt="link" className="list-group-item"> Figma</a>
+          <a href="https://codepen.io/" alt="link" className="list-group-item"> CodePen</a>
+          <a href="https://coolors.co/" alt="link" className="list-group-item"> Coolors</a>
+          <a href="https://pt-br.reactjs.org/docs/getting-started.html" alt="link" className="list-group-item"> ReactJS</a>
+          <a href="https://developer.mozilla.org/pt-BR/" alt="link" className="list-group-item"> MDN</a>
         </ul>
       </div>
     </div>
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,13 +1,46 @@
 import "./SearchBar.css";
 import { HiSearch } from "react-icons/hi";
+import { useState } from "react";
+
 const SearchBar = (props) => {
+  const [content, setContent] = useState("");
+
+  const handleChange = (e) => {
+    setContent(e.target.value);
+  };
+
+  const handleClick = () => {
+    if(content.length <= 0) return;
+    
+    if(content.split("/")[0] === "g"){
+      window.open(
+        `https://www.google.com/search?q=${content}`,
+        "_blank"
+      );
+    }
+    else if(content.split("/")[0] === "dg"){
+      window.open(
+        `https://duckduckgo.com/?q=${content}`,
+        "_blank"
+      );
+    }
+    setContent("");
+  };
+
+  const handleKeyDown = e => {
+    if (e.key === "Enter") {
+      handleClick();
+    };
+  };
+
   return (
     <div className="card div-search-bar">
-      <button className="btn-search-bar">
+      <button className="btn-search-bar" onClick={handleClick}>
         <HiSearch color="#38BDF8"/>
       </button>
-      <input className="input-search-bar" type="text" placeholder="g/you-search-here" />
+      <input className="input-search-bar" type="text" placeholder="g/you-search-here" value={content} onChange={handleChange} onKeyDown={handleKeyDown}/>
     </div>
   );
 };
+
 export default SearchBar;
