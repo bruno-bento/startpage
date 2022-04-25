4ff313fb1cf36b8d16b1eea140bfea6b36cd144b
bruno-bento <brunobentoreinoso14@gmail.com>
2022-04-25 16:55:34 -0300

    DATA

---
+++ src/components/data/data.js
@@ -0,0 +1,75 @@
+export const themes = {
+    "id": "default",
+    "title": "Olá, Bruno",
+    "imagem": "exemple.png",
+    "color": [
+        {
+            "title": "Data",
+            "--bg-color": "#0E162D",
+            "--bg-cards-color": "#1E293B",
+            "--shadow-color": "#314462",
+            "--spotlight-text-color": "#38BDF8",
+            "--deafult-text-color": "#FFFFFF",
+        }
+    ]
+}
+
+
+export const links = {
+    title: "Dev",
+    links: [
+        {
+            title: "Github",
+            url: "https://github.com/bruno-bento",
+            shortcut: "gh",
+        },
+        {
+            title: "StackOverflow",
+            url: "https://stackoverflow.com/",
+            shortcut: "so",
+        },
+        {
+            title: "Figma",
+            url: "https://www.figma.com/files/",
+            shortcut: "",
+        },
+        {
+            title: "CodePen",
+            url: "https://codepen.io/",
+            shortcut: "cp",
+        },
+        {
+            title: "Coolors",
+            url: "https://coolors.co/",
+            shortcut: "cl",
+        }
+    ],
+    title: "Reddirt",
+    links: [
+        {
+            title: "Github",
+            url: "https://github.com/bruno-bento",
+            shortcut: "gh",
+        },
+        {
+            title: "StackOverflow",
+            url: "https://stackoverflow.com/",
+            shortcut: "so",
+        },
+        {
+            title: "Figma",
+            url: "https://www.figma.com/files/",
+            shortcut: "",
+        },
+        {
+            title: "CodePen",
+            url: "https://codepen.io/",
+            shortcut: "cp",
+        },
+        {
+            title: "Coolors",
+            url: "https://coolors.co/",
+            shortcut: "cl",
+        }
+    ]
+}
